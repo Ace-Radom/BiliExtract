5a27fabb945908633d939e8023be96f28f228490
Ace_Radom <sichenradomlyu@gmail.com>
2025-01-16 12:14:28 +0100

    ADB socket error handling rework

--- BiliExtract.Lib/Adb/AdbSocket.cs
+++ BiliExtract.Lib/Adb/AdbSocket.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -10,17 +11,28 @@ public class AdbSocket : IDisposable
 {
     private readonly Encoding _encoding = Encoding.ASCII;
 
-    private readonly TcpClient _tcpClient;
-    private readonly NetworkStream _tcpStream;
+    private readonly TcpClient? _tcpClient;
+    private readonly NetworkStream? _tcpStream;
 
-    private byte[] _buf = new byte[65536];
+    private readonly byte[] _buf = new byte[65536];
 
-    public AdbSocket(string host, int port)
+    private string? _lastError;
+
+    public string? LastError => _lastError;
+
+    public AdbSocket(IPAddress host, int port)
     {
         Log.GlobalLogger.WriteLog(LogLevel.Info, $"Connecting to ADB server. [host={host},port={port}]");
-        _tcpClient = new TcpClient(host, port);
-        _tcpStream = _tcpClient.GetStream();
-
+        try
+        {
+            _tcpClient = new TcpClient(host.ToString(), port);
+            _tcpStream = _tcpClient.GetStream();
+        }
+        catch (Exception ex)
+        {
+            Log.GlobalLogger.WriteLog(LogLevel.Warning, $"Failed to connect to ADB server.", ex);
+            _lastError = $"Failed to connect to ADB server: {ex.Message}";
+        }
         return;
     }
 
@@ -33,89 +45,133 @@ public class AdbSocket : IDisposable
         return;
     }
 
-    public void Write(byte[] data, int size)
+    public int Write(byte[] data, int size)
     {
-        Log.GlobalLogger.WriteLog(LogLevel.Debug, $"Write to ADB socket. [size={size}]");
+        _lastError = null;
+        if (_tcpStream is null)
+        {
+            _lastError = "Write to null TCP stream";
+            return -1;
+        }
+
         _tcpStream.Write(data, 0, size);
-        return;
+        Log.GlobalLogger.WriteLog(LogLevel.Debug, $"Wrote to ADB socket. [size={size}]");
+        return size;
     }
 
-    public void Write(byte[] data)
+    public int Write(byte[] data)
     {
-        Write(data, data.Length);
-        return;
+        return Write(data, data.Length);
     }
 
-    public void WriteString(string text)
+    public int WriteString(string text)
     {
         var size = _encoding.GetBytes(text, 0, text.Length, _buf, 0);
-        Write(_buf, size);
-        return;
+        return Write(_buf, size);
     }
 
-    public void WriteSyncString(string text)
+    public int WriteSyncString(string text)
     {
-        WriteInt(text.Length);
-        WriteString(text);
-        return;
+        int size = 0;
+        size += WriteInt(text.Length);
+        size += WriteString(text);
+        return size;
     }
 
-    public void WriteSyncStringHex(string text)
+    public int WriteSyncStringHex(string text)
     {
-        WriteString($"{text.Length:X04}");
-        WriteString(text);
-        return;
+        int size = 0;
+        size += WriteString($"{text.Length:X04}");
+        size += WriteString(text);
+        return size;
     }
 
-    public void WriteInt(int num)
+    public int WriteInt(int num)
     {
         var bytes = BitConverter.GetBytes(num);
-        Write(bytes);
-        return;
+        return Write(bytes);
     }
 
-    public void Read(byte[] data, int size)
+    public int Read(byte[] data, int size)
     {
+        _lastError = null;
+        if (_tcpStream is null)
+        {
+            _lastError = "Read from null TCP stream";
+            return -1;
+        }
+        if (size < 0)
+        {
+            _lastError = "Illegal read size";
+            return -1;
+        }
+
         var readCount = 0;
         while (readCount < size)
         {
             readCount += _tcpStream.Read(data, readCount, size - readCount);
         }
-        return;
+        Log.GlobalLogger.WriteLog(LogLevel.Debug, $"Read from ADB socket [size={readCount}]");
+        return readCount;
     }
 
     public string ReadString(int length)
     {
-        Read(_buf, length);
+        if (Read(_buf, length) == -1)
+        {
+            return string.Empty;
+        }
         return _encoding.GetString(_buf, 0, length);
     }
 
     public string ReadSyncString()
     {
         var length = ReadInt();
+        if (length < 0)
+        {
+            return string.Empty;
+        }
         return ReadString(length);
     }
 
     public string ReadSyncStringHex()
     {
         var length = ReadIntHex();
+        if (length < 0)
+        {
+            return string.Empty;
+        }
         return ReadString(length);
     }
 
     public int ReadInt()
     {
-        Read(_buf, 4);
+        if (Read(_buf, 4) == -1)
+        {
+            return int.MinValue;
+        }
         return BitConverter.ToInt32(_buf, 0);
     }
 
     public int ReadIntHex()
     {
         var hex = ReadString(4);
+        if (string.IsNullOrEmpty(hex))
+        {
+            return int.MinValue;
+        }
         return Convert.ToInt32(hex, 16);
     }
 
     public string[] ReadAllLines()
     {
+        _lastError = null;
+        if (_tcpStream is null)
+        {
+            _lastError = "Read from null TCP stream";
+            return [];
+        }
+
         var lines = new List<string>();
         using var reader = new StreamReader(_tcpStream, _encoding);
         while (true)
@@ -130,41 +186,70 @@ public class AdbSocket : IDisposable
         return lines.ToArray();
     }
 
-    public void SendCommand(string command)
+    public int ExecuteCommand(string command)
     {
-        WriteSyncStringHex(command);
+        Log.GlobalLogger.WriteLog(LogLevel.Info, $"Executing ADB server command. [cmd=\"{command}\"]");
+
+        _lastError = null;
+        int ret = WriteSyncStringHex(command);
+        if (ret == -1)
+        {
+            return -1;
+        }
         var response = ReadString(4);
+        if (string.IsNullOrEmpty(response))
+        {
+            return -1;
+        }
         switch (response)
         {
             case "OKAY":
-                return;
+                return 0;
             case "FAIL":
                 var message = ReadSyncStringHex();
+                if (string.IsNullOrEmpty(message))
+                {
+                    return -1;
+                }
+                _lastError = $"Command execute failed: {message}";
                 Log.GlobalLogger.WriteLog(LogLevel.Warning, $"ADB server responsed failed. [cmd=\"{command}\",msg=\"{message}\"]");
-                throw new InvalidOperationException("ADB server responsed failed");
+                return 1;
             default:
-                throw new InvalidOperationException("Unexcepted ADB response");
+                _lastError = $"Unexpected response: {response}";
+                Log.GlobalLogger.WriteLog(LogLevel.Warning, $"ADB server responsed unexpectedly. [cmd=\"{command}\",response=\"{response}\"]");
+                return 1;
         }
     }
 
-    public string SendSyncCommand(string command, string parameter, bool readResponse = true)
+    public int ExecuteSyncCommand(string command, string parameter, out string response)
     {
-        WriteString(command);
-        WriteSyncString(parameter);
+        Log.GlobalLogger.WriteLog(LogLevel.Info, $"Executing ADB server command. [cmd=\"{command}\"]");
 
-        if (!readResponse)
+        _lastError = null;
+        response = string.Empty;
+        int ret = WriteString(command);
+        if (ret == -1)
         {
-            return string.Empty;
+            return -1;
+        }
+        ret = WriteSyncString(parameter);
+        if (ret == -1)
+        {
+            return -1;
         }
 
-        var response = ReadString(4);
+        response = ReadString(4);
+        if (string.IsNullOrEmpty(response))
+        {
+            return -1;
+        }
         if (response == "FAIL")
         {
-            var message = ReadSyncStringHex();
-            Log.GlobalLogger.WriteLog(LogLevel.Warning, $"ADB server responsed failed. [cmd=\"{command}\",msg=\"{message}\"]");
-            throw new InvalidOperationException("ADB server responsed failed");
+            response = ReadSyncStringHex();
+            Log.GlobalLogger.WriteLog(LogLevel.Warning, $"ADB server responsed failed. [cmd=\"{command}\",msg=\"{response}\"]");
+            return 1;
         }
 
-        return response;
+        return 0;
     }
 }
